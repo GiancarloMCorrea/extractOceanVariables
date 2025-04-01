8ed30625cb2898c58adb993b214e978c17b176dd
NagoreC <ncuevas@azti.es>
2025-04-01 11:11:48 +0200

    update bathy

--- code/bathy/mainBATHY.R
+++ code/bathy/mainBATHY.R
@@ -22,7 +22,7 @@ lonlat_cols <- c("Lon_M", "Lat_M")
 
 # -------------------------------------------------------------------------
 # Get bathymetry information for each observation: 
-envData = extractBATHY(data = mainDat, lonlat_cols = lonlat_cols)
+envData = extractBATHY(data = mainDat, lonlat_cols = lonlat_cols)#agregar
 
 # Save new data with environmental information:
 write.csv(envData, file = file.path('data', paste0("data_with_bathy.csv")), row.names = FALSE)
